bdf99cda330fcf5387510c67ea6465c6b8974362
Daniel Karnofel <91570684+danielkarnofel@users.noreply.github.com>
2022-06-08 14:22:39 -0700

    Add files via upload

---
+++ projects/01_Covid_Destroyer/destroyer.js
@@ -0,0 +1,447 @@
+// Art 102MM - Project #3
+// Covid Destroyer Game v2.0
+// Daniel Karnofel
+
+// images made in Illustrator
+
+// - - - - - - - - - - - - - - - - - - - - 
+// declare global variables
+
+// image variables
+let laser, virus;
+
+// font variables
+let marker, roboto;
+
+// create array of virus mobs
+let mobs = [];
+let num = 5; // # of mobs
+
+// castle variables
+let wallSize = 70; // 
+let wallLife = 25; // 
+
+// laser variables
+let laserX, laserY;
+let fire;
+
+// laser sound
+let osc, freq, amp, mod;
+
+// game variables
+let score, playing;
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function preload(){
+  
+  // load images
+  laser = loadImage('images/laser_cannon.png');
+  virus = loadImage('images/virus.png');
+  
+  // load fonts
+  marker = loadFont('../../fonts/PermanentMarker-Regular.ttf');
+  roboto = loadFont('../../fonts/Roboto-Regular.ttf');
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function setup() {
+  // let wWidth = windowHeight * .75;
+  // var canvas = createCanvas(wWidth, windowHeight);
+
+  var canvas = createCanvas(450, 600);
+  canvas.parent('sketch-div');
+  
+  cursor(CROSS);
+  
+  // laser sound
+  canvas.mousePressed(laserSound);
+  osc = new p5.Oscillator('sine');
+  mod = 0;
+  
+  // create virus mobs
+  for(let i = 0; i < num; i++){
+    mobs.push(new Virus());
+  }
+  
+  score = 0;
+  playing = false;
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function draw() {
+  background(80, 100, 50);
+  
+  // draw virus mobs
+  for(let i = 0; i < mobs.length; i++){
+    mobs[i].move();
+    mobs[i].damage();
+    mobs[i].attack();
+    mobs[i].display();
+  }
+  
+  // - - - - - - - - - - - - - - - - - - - - 
+  
+  // draw castle
+  createCastle();
+  
+  // - - - - - - - - - - - - - - - - - - - - 
+  
+  // draw laser cannon
+  createLaser();
+  
+  // laser sound
+  if(fire){
+    osc.freq(freq, 0.1);
+    osc.amp(0.5, 0.1);
+  }
+  
+  // game messages
+  messages();
+  
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+// interactivity
+
+// function keyPressed(){
+  
+//   if(playing == false){
+    
+//     playing = true;
+    
+//   }
+  
+// }
+
+function mousePressed(){
+
+  if(playing == false){
+    
+    playing = true;
+    
+  }
+  
+  fire = true;
+  
+  if(wallLife <= 0){
+    
+    wallLife = 25;
+    score = 0;
+    playing = false;
+    
+    for(let i = 0; i < mobs.length; i++){
+      
+      mobs[i].x = random(width);
+      mobs[i].y = random(-25, -height);
+      mobs[i].speed = 0;
+      
+      
+    }
+    
+  }
+  
+}
+
+function mouseReleased(){
+  
+  fire = false;
+  
+  osc.amp(0, 0.5);
+  
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+// virus class
+class Virus{
+  
+  constructor(){
+    this.x = random(width);
+    this.y = random(-50, -height);
+    this.size = 50;
+    this.speed = 0;
+    this.hp = 20;
+    this.hit = false;
+  }
+  
+  // - - - - - - - - - - - - - - - - - - - - 
+  
+  move(){
+    
+    if(this.hp > 0){
+      
+      this.y += this.speed;
+      
+      this.x += random(-1, 1);
+      
+    }   
+    
+    if(playing == true){
+      this.speed = 1 + score/20;
+    } else {
+      this.speed = 0;
+    }
+    
+    
+  }
+  
+  // - - - - - - - - - - - - - - - - - - - - 
+  
+  damage(){
+    
+    // get laser beam line equation
+    let lX = mouseX - laserX;
+    let lY = laserY - mouseY;
+    let m = lY/(lX); // slope of laser beam
+    if(abs(m) > 150){
+      m = 150;
+    }
+    
+    // convert mob coordinates
+    let mX = this.x - laserX;
+    let mY = laserY - this.y;
+    
+    // check distance from mob to laser beam
+    let d = abs(mY - mX*m);
+    
+    if(d < this.size+5 && fire == true && wallLife > 0){
+      this.hit = true;
+    } else if(abs(lX) < 10 && abs(mX) < 10 && fire == true && wallLife > 0){
+      this.hit = true;
+    } else {
+      this.hit = false;
+    }
+    
+    // inflict damage
+    if(this.hit == true){
+      
+      this.hp -= 1;
+      
+    }
+    
+    // respawn when killed
+    if(this.hp <= 0){
+      
+      this.y = random(0-this.size, -height);
+      this.x = random(this.size, width-this.size);
+      
+      this.hp = 10;
+      
+      score++;
+      
+    }
+    
+  }
+  
+  // - - - - - - - - - - - - - - - - - - - - 
+  
+  attack(){
+    
+    // collision detection with castle
+    if(this.y >= height - wallSize && wallLife > 0){
+      
+      // damage castle wall
+      wallLife -= 1;
+      
+      // respawn new mob
+      this.y = random(0-this.size, -height);
+      this.x = random(this.size, width-this.size);
+      
+    } 
+    
+    // respawn mobs after castle is destroyed
+    else if (this.y > height && wallLife <= 0){
+      
+      this.y = random(0-this.size, -height);
+      this.x = random(this.size, width-this.size);
+      
+    }
+    
+  }
+  
+  // - - - - - - - - - - - - - - - - - - - - 
+  
+  display(){
+    
+    if(this.hp > 0 && this.hit == false){
+      
+      imageMode(CENTER);
+      image(virus, this.x, this.y, this.size, this.size);
+      
+    } else if(this.hp > 0 && this.hit == true){
+      
+      strokeWeight(5);
+      stroke(100, 0, 255, 100);
+      fill(255, 0, 100, 100);
+      circle(this.x, this.y, this.size);
+      
+      imageMode(CENTER);
+      image(virus, this.x, this.y, this.size, this.size);
+      
+    }
+    
+  }
+  
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function createCastle(){
+  
+  // draw castle
+  push();
+  if(wallLife > 0){
+    
+    fill(100);
+    noStroke();
+    rect(0, height-wallSize, width, wallSize); 
+    
+    stroke(0);
+    strokeWeight(3);
+    line(0, height-wallSize, width, height-wallSize);
+    
+  }
+  pop();
+  
+  // draw health bar
+  push();
+  let hColor = map(wallLife, 0, 25, 0, 120);
+  colorMode(HSB);
+  noStroke();
+  fill(hColor, 255, 255);
+  rectMode(CENTER);
+  rect(width/2, height-wallSize/4, wallLife*10, 5);
+  pop();
+  
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function createLaser(){
+  
+  laserX = width/2;
+  laserY = height-wallSize;
+  
+  push();
+  
+  // translate to bottom-middle and follow mouse
+  translate(laserX, laserY);
+  let a = atan2(mouseY-laserY, mouseX-laserX);
+  rotate(a+PI*0.491);
+  
+  if(wallLife > 0){
+    
+    // draw laser beam
+    if(fire == true){
+    
+      strokeWeight(5);
+      stroke(255, 0, 0);
+      line(0, 0, 0, -height*1.1);
+
+      strokeWeight(15);
+      stroke(255, 0, 0, 50);
+      line(0, -50, 0, -height*1.1);
+    
+    }
+    
+    // draw laser cannon
+    imageMode(CENTER);
+    image(laser, 0, 0, 90, 100);
+    
+  }
+  
+  pop();
+  
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function laserSound(){
+
+  mod++;
+  let freq = map(sin(radians(mod)), -1, 1, 180, 220);
+
+  osc.freq(freq, 0.1);
+  osc.amp(0.1);
+  osc.start();
+  
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function messages(){
+  
+  strokeWeight(5);
+  stroke(100, 0, 255, 100);
+  textFont(roboto);
+  
+  if(playing == false){
+    
+    textAlign(CENTER, CENTER);
+    
+    let title1 = 'COVID';
+    let title2 = 'DESTROYER';
+    
+    push();
+    textFont(marker);
+    textSize(40);
+    fill(255, 0, 0);
+    text(title1, width/2, height/2-20);
+    text(title2, width/2, height/2+20);
+    pop();
+    
+    let playText = 'click to begin';
+    
+    textSize(15);
+    fill(255);
+    text(playText, width/2, height*(2/3));
+    
+  }
+  
+  let scoreText = score;
+  
+  textAlign(LEFT, CENTER);
+  textSize(15);
+  fill(255);
+  text(scoreText, width/2, 25);
+  
+  if(wallLife <= 0){
+    
+    let t1 = "oh no! you've been";
+    let t2 = 'CONTAMINATED';
+    let t3 = "go wash your hands";
+    let t4 = "click to try again";
+    
+    textAlign(CENTER, CENTER);
+    
+    textSize(15);
+    fill(255);
+    text(t1, width/2, height/2);
+    
+    push();
+    textFont(marker);
+    textSize(25);
+    fill(255, 0, 0);
+    text(t2, width/2, height/2+25);
+    pop();
+    
+    textSize(15);
+    fill(255);
+    text(t3, width/2, height/2+50);
+    
+    textSize(10);
+    fill(255);
+    text(t4, width/2, height/2+75)
+    
+  }
+  
+}
+
+
+
+// - - - - - - - - - - - - - - - - - - - - 
---
+++ projects/02_Many_Worlds/sketch.js
@@ -0,0 +1,659 @@
+// Art 102MM - Final Project
+// Higher Dimensional Geometries - v3.0
+// Daniel Karnofel
+// - - - - - - - - - - - - - - - - - - - - 
+
+// switches
+let r = 200;
+let detail = 20;
+
+let start = false;
+let showInfo = false;
+let button;
+
+let infoText = "Many Worlds explores the idea of higher dimensional space and geometry through an intuitive exploratory shape generator. ";
+    infoText += "Each generated 3D shape is just one of thousands of 'slices' composing a shape that exists in 6 dimensions. ";
+    infoText += "By exploring the many possible 3D 'slices' the user can gain an intuition of the extension of this shape into the higher dimensions. ";
+    infoText += "The 3 familiar spatial dimensions are represented by the width, height, and depth of the visible 3D shape, while the remaining 3 dimensions are expressed by mouse movement and cyclical time. ";
+    infoText += "The generated geometry in this project is created with the superformula proposed by Johan Gielis, a generalization of the superellipse. ";
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function preload(){
+
+  font1 = loadFont('../../fonts/Raleway-ExtraBold.ttf');
+  font2 = loadFont('../../fonts/Roboto-Regular.ttf');
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function setup() {
+
+  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  canvas.parent('sketch-div');
+  
+  // createCanvas(windowWidth, windowHeight, WEBGL);
+
+  cursor(CROSS);
+
+  // replace w/ ux:
+  
+  // supershape 1 parameters:
+  n1 = 1; 
+  n2 = 1; 
+  n3 = 1;
+  a1 = 1; 
+  b1 = 1; 
+  m1 = 4;
+
+  // supershape 2 parameters:
+  n4 = 1;
+  n5 = 1;
+  n6 = 1;
+  a2 = 1;
+  b2 = 1;
+  m2 = 4;
+
+  // supershape 3 parameters:
+  n7 = 1; 
+  n8 = 1; 
+  n9 = 1;
+  a3 = 1;
+  b3 = 1;
+  m3 = 0;
+
+  // - - - - - - - - - - 
+  // create buttons:
+
+  startButton = createButton('start');
+  startButton.parent('button');
+  startButton.size(50, AUTO);
+  startButton.mousePressed(startUp);
+
+  endButton = createButton('end');
+  endButton.parent('button');
+  endButton.size(50, AUTO);
+  endButton.mousePressed(restart);
+  endButton.hide();
+  
+  infoButton = createButton('info');
+  infoButton.parent('button');
+  infoButton.size(50, AUTO);
+  infoButton.mousePressed(info);
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function startUp(){
+
+  start = true;
+  showInfo = false;
+
+  startButton.hide();
+  infoButton.hide();
+
+  endButton.show();
+
+  setupSound();
+
+}
+
+function info(){
+
+  if(showInfo){showInfo=false}
+  else if(!showInfo){showInfo=true}
+
+}
+
+function restart(){
+
+  start = false;
+  let info = false;
+
+  endButton.hide();
+
+  startButton.show();
+  infoButton.show();
+
+  stopSound();
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function draw() {
+  
+  colorMode(RGB);
+  background(230, 237, 236);
+
+  // line(0, -height/2, 0, height/2);
+
+  startButton.position(width/2-18, height/2+70);
+  endButton.position(50, 50);
+  infoButton.position(width/2-18, height/2+100);
+
+  // - - - - - - - - - - 
+
+  if(showInfo == true && start == false){
+
+    push();
+    textSize(15);
+    textLeading(20);
+    textFont(font2);
+    textAlign(LEFT, TOP);
+
+    colorMode(RGB);
+    fill(100);
+    text(infoText, -300, 150, 600, 230);
+    pop();
+
+  }
+  
+  // - - - - - - - - - - 
+
+  if(start == false){
+    // - - - - - - - - - - - - - - - - - - - - 
+  
+    wavyTitle();
+
+  } else if(start == true) {
+
+    createShape();
+  
+    playSound();
+  
+    lighting();
+
+    shapeCo();
+  
+    drawShape();
+
+    outlines();
+
+  }
+  
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function shapeCo(){
+
+  push();
+
+  textSize(15);
+  textFont(font2);
+  textAlign(LEFT, CENTER);
+
+  colorMode(RGB);
+  fill(100);
+
+  push();
+
+  let c1 = round(m1, 2);
+  let c2 = round(m2, 2);
+  let c3 = round(map(l3, 0, PI, 0, 10), 2);
+
+  text('v:', -70, height/2-25);
+  text('w:', -70, height/2-45);
+  text('t:', -70, height/2-65);
+
+  text(c1, -50, height/2-25);
+  text(c2, -50, height/2-45);
+  text(c3, -50, height/2-65);
+
+  pop();
+
+  // - - - - - - - - - - 
+
+  push();
+  textSize(15);
+  textAlign(CENTER, CENTER);
+  text('higher dimensional coordinates:', 0, height/2-90);
+  pop();
+
+  pop();
+
+  // - - - - - - - - - - 
+
+  push();
+
+  colorMode(RGB);
+
+  translate(30, height/2-45);
+
+  // rotateX(-PI/8);
+  rotateY(PI/10);
+
+  let size = 35;
+  let tC = map(PI/2 - l3, PI/2, -PI/2, 0, PI);
+  let t = map(tC, 0, PI, -size/2+1, size/2-1, true);
+  let v = map(mouseX, 0, width, -size/2+1, size/2-1, true);
+  let w = map(mouseY, 0, height, -size/2+1, size/2-1, true);
+  
+  push();
+  strokeWeight(7);
+  stroke(50, 50, 0, 30);
+  point(v, w, t);
+  pop();
+  
+  push();
+  translate(0, 0, t);
+  noStroke();
+  fill(0, 0, 200, 100);
+  plane(size, size);
+  pop();
+  
+  push();
+  stroke(0, 100);
+  fill(200, 0, 200, 50);
+  box(size);
+  pop();
+
+  pop();
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function outlines(){
+
+	// - - - - - - - - - - - - - - - - - - - - 
+	// supershape outlines
+
+	push();
+
+	let oRad = 50;
+
+	noFill();
+	stroke(50);
+	strokeWeight(0.5);
+
+	// - - - - - - - - - - - - - - - 
+
+	push();
+
+	translate(-width/2+1.5*oRad, height/2-1.5*oRad);
+	beginShape();
+	for(let i = 0; i < detail; i++){
+
+		let lat = map(i, 0, detail, 0, TWO_PI);
+    	x = oRad * r1[i] * cos(lat);
+    	y = oRad * r1[i] * sin(lat);
+
+    	vertex(x, y, 0);
+
+  	}
+  	endShape(CLOSE);
+  	pop();
+
+	// - - - - - - - - - - - - - - - 
+
+	push();
+
+	translate(width/2-1.5*oRad, height/2-1.5*oRad);
+	beginShape();
+	for(let i = 0; i < detail; i++){
+
+	    let lat = map(i, 0, detail, 0, TWO_PI);
+	    x = oRad * r2[i] * cos(lat);
+	    y = oRad * r2[i] * sin(lat);
+
+	    vertex(x, y, 0);
+
+	}
+	endShape(CLOSE);
+	pop();
+
+	// - - - - - - - - - - - - - - - 
+
+	push();
+
+	translate(0, height/2-1.5*oRad);
+	beginShape();
+	for(let i = 0; i < detail; i++){
+
+		let lat = map(i, 0, detail, 0, TWO_PI);
+    	x = oRad * r3[i] * cos(lat);
+    	y = oRad * r3[i] * sin(lat);
+
+    	vertex(x, y, 0);
+
+  	}
+  	endShape(CLOSE);
+  	pop();
+
+  	pop();
+
+
+}
+
+// vector array
+let shape = [];
+let x, y, z;
+
+// radius arrays
+let r1 = [];
+let r2 = [];
+let r3 = [];
+
+let l1, l2, l3;
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function createShape(){
+
+  // morph controls
+  m1 = map(mouseX, 0, width, 0, 10, true);
+  m2 = map(mouseY, 0, height, 0, 10, true);
+  
+  
+  // create vector array
+  for(let i = 0; i< detail+1; i++){
+    
+    shape[i] = [];
+    
+    // r3[i] = [];
+    
+    l1 = map(i, 0, detail, -PI, PI);
+    
+    r1[i] = supershape1(l1, a1, b1, m1, n1, n2, n3);
+    
+    for(let j = 0; j < detail+1; j++){
+      
+      l2 = map(j, 0, detail, -PI, PI);
+      
+      r2[j] = supershape2(l2, a2, b2, m2, n4, n5, n6);
+      
+      let k = sin(millis()/5000);
+        
+      l3 = map(k, -1, 1, 0, PI);
+      
+      m3 = abs(map(j, 0, detail, -10, 10));
+      
+      r3[k] = supershape3(l3, a3, b3, m3, n7, n8, n9);
+      
+      x = r * r3[k]*cos(l3) * r2[j]*cos(l2) * r1[i]*cos(l1);
+      y = r * r3[k]*cos(l3) * r2[j]*cos(l2) * r1[i]*sin(l1);
+      z = r * r3[k]*cos(l3) * r2[j]*sin(l2);
+      
+      shape[i][j] = createVector(x, y, z);
+      
+    }
+  }
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function drawShape(){
+
+  // draw shape
+  // - - - - - - - - - - 
+  
+  push();
+  
+  // rotation: 
+  rotateX(PI/3);
+  rotateZ(millis()/1500);
+  
+  for(let i = 0; i < detail; i++){
+    
+    beginShape(TRIANGLE_STRIP);
+    
+    for(let j = 0; j < detail+1; j++){
+
+      noStroke();
+         
+      let v1 = shape[i][j];
+      vertex(v1.x, v1.y, v1.z);
+      
+      let v2 = shape[i+1][j];
+      vertex(v2.x, v2.y, v2.z);
+      
+    }  
+    
+    endShape();
+    
+  }
+  
+  pop();
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function lighting(){
+
+  // lighting & material:
+  // - - - - - - - - - - 
+
+  colorMode(RGB);
+  
+  specularMaterial(255, 255, 255);
+
+  colorMode(HSB);
+  
+  // back light:
+  pointLight(10, 255, 120, -r/2, -1.5*r, -2*r);
+  
+  // key light:
+  pointLight(250, 255, 240, 2*r, -2*r, r);
+  
+  // fill light:
+  pointLight(90, 255, 180, -2*r, -r, r);
+
+  shininess(20);
+
+  
+  
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+// Sound Generation
+
+let osc = [];
+let freq = [];
+let mag = [];
+
+let waves = 8;
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function setupSound(){
+  
+  for(let i = 0; i < waves; i++){
+    
+    osc[i] = [];
+    freq[i] = [];
+    mag[i] = [];
+    
+    for(let j = 0; j < waves; j++){
+      
+      osc[i][j] = new p5.Oscillator('sine');
+    
+      osc[i][j].freq(0);
+      osc[i][j].amp(0.1);
+  
+      osc[i][j].start();
+      
+    }   
+  }
+  
+}
+
+// - - - - - - - - - - 
+
+function playSound(){
+  
+  for(let i = 0; i < waves; i++){
+    
+    let s = floor(map(i, 0, waves, 0, detail));
+    
+    for(let j = 0; j < waves; j++){
+      
+      let t = floor(map(j, 0, waves, 0, detail));
+      
+      mag[i][j] = shape[s][t].mag()/r;
+      
+      freq[i][j] = map(mag[i][j], 0, 1, 75, 300);
+      
+      osc[i][j].freq(freq[i][j], 0.1);
+      
+    }
+  }
+  
+}
+
+function stopSound(){
+
+  for(let i = 0; i < waves; i++){
+    
+    for(let j = 0; j < waves; j++){
+  
+      osc[i][j].stop();
+      
+    }   
+  }
+
+}
+
+
+// supershape 1 parameters:
+let n1, n2, n3;
+let a1, b1, m1;
+
+// supershape 2 parameters:
+let n4, n5, n6;
+let a2, b2, m2;
+
+// supershape 3 parameters:
+let n7, n8, n9;
+let a3, b3, m3;
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function supershape1(t, a1, b1, m1, n1, n2, n3){
+  
+  let t1 = abs((1/a1) * cos(m1*t/4));
+  t1 = pow(t1, n2);
+  
+  let t2 = abs((1/b1) * sin(m1*t/4));
+  t2 = pow(t2, n3);
+  
+  let t3 = t1 + t2;
+  
+  let r1 = constrain(pow(t3, -1 / n1), 0, 1);
+
+  return r1;
+  
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function supershape2(t, a2, b2, m2, n4, n5, n6){
+  
+  let t1 = abs((1/a2) * cos(m2*t/4));
+  t1 = pow(t1, n5);
+  
+  let t2 = abs((1/b2) * sin(m2*t/4));
+  t2 = pow(t2, n6);
+  
+  let t3 = t1 + t2;
+  
+  let r2 = constrain(pow(t3, -1 / n4), 0, 1);
+
+  return r2;
+  
+}
+
+// - - - - - - - - - - - - - - - - - - - - 
+
+function supershape3(t, a3, b3, m3, n7, n8, n9){
+  
+  let t1 = abs((1/a3) * cos(m3*t/4));
+  t1 = pow(t1, n8);
+  
+  let t2 = abs((1/b3) * sin(m3*t/4));
+  t2 = pow(t2, n9);
+  
+  let t3 = t1 + t2;
+  
+  let r3 = constrain(pow(t3, -1 / n7), 0, 1);
+
+  return r3;
+  
+}
+
+let title = 'MANY  WORLDS';
+let tx, ty;
+
+function wavyTitle(){
+
+    textFont(font1);
+    textAlign(CENTER, CENTER);
+
+    textSize(60);
+
+    let scl = 2;
+    let rate;
+    
+    push();
+    rate = 600;
+    tx = scl*sin(millis()/rate);
+    ty = scl*cos(millis()/rate);
+    translate(tx, ty);
+    fill(255, 0, 0, 100);
+    text(title, 0, -50);
+    pop();
+    
+    push();
+    rate = 500;
+    tx = scl*sin(millis()/rate+PI/3);
+    ty = scl*cos(millis()/rate+1);
+    translate(tx, ty);
+    fill(0, 255, 0, 90);
+    text(title, 0, -50);
+    pop();
+    
+    push();
+    rate = 400;
+    tx = scl*sin(millis()/rate+TWO_PI/3);
+    ty = scl*cos(millis()/rate+2);
+    translate(tx, ty);
+    fill(0, 0, 255, 80);
+    text(title, 0, -50);
+    pop();
+
+    // - - - - - - - - - - 
+
+    push();
+    textSize(15);
+    textFont(font2);
+    textAlign(CENTER, CENTER);
+    fill(100);
+    text('an explorable higher dimensional shape', 0, 0);
+    pop();
+
+    // - - - - - - - - - - 
+
+    push();
+    textSize(10);
+    textFont(font2);
+    textAlign(CENTER, CENTER);
+    fill(100);
+    text('created by Daniel Karnofel', 0, 25);
+    pop();
+
+}
+
+
+
+
+
+
+
+
